71e8c9021b99825c76f4fb0e4c77e977f71f9def
sandhiyaramesh <sandyramesh920@gmail.com>
2021-01-29 10:15:20 +0530

    NewCode

---
+++ src/test/java/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+    public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
+		System.out.println("Test7");
+		System.out.println("Test8");
+	}
+}
